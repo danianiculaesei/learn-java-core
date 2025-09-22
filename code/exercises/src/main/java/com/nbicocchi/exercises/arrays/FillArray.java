83bb047722ac06d52d62640f083741ce52f49b65
Daniel Aniculaesei <145033708+danianiculaesei@users.noreply.github.com>
2025-09-22 15:37:29 +0200

    fillArray method for improved noise handling
    Everything is handled in a single loop and you don't have to do extra passes over the array.
    Also each element is calculated directly with or without the noise inside the random.nextDouble.
    The logic is easy understandable: if noise is enabled, generate a random value within ±5%, otherwise, assign the fixed value.

--- code/exercises/src/main/java/com/nbicocchi/exercises/arrays/FillArray.java
+++ code/exercises/src/main/java/com/nbicocchi/exercises/arrays/FillArray.java
@@ -1,18 +1,18 @@
 package com.nbicocchi.exercises.arrays;
 
-import java.util.Arrays;
 import java.util.random.RandomGenerator;
 
 public class FillArray {
-    public static double[] fillArray(int size, double value, boolean addNoise) {
-        double[] dst = new double[size];
-        Arrays.fill(dst, value);
-        if (addNoise) {
-            RandomGenerator rnd = RandomGenerator.getDefault();
-            for (int i = 0; i < dst.length; i++) {
-                dst[i] += rnd.nextDouble(-value, value) * 0.05;
+    public static double[] fillArray(int size, double value, boolean addNoise){
+        RandomGenerator random = RandomGenerator.getDefault();
+        double[] array = new double[size];
+        for(int i = 0; i < size; i++){
+            if(addNoise){
+                array[i] = random.nextDouble(value - (value * 0.05), value + (value *0.05 ));
+            }else {
+                array[i] = value;
             }
         }
-        return dst;
+        return array;
     }
 }
